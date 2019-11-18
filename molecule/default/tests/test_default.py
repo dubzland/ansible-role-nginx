9ca3938433a564d9280af78fee10902c05a6230d
Josh Williams <vmizzle@gmail.com>
2019-11-18 12:05:03 -0600

    Add some rudimentary infra tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,9 +7,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_nginx_is_installed(host):
+    nginx = host.package('nginx')
+    assert nginx.is_installed
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_nginx_running_and_enabled(host):
+    nginx = host.service('nginx')
+    assert nginx.is_running
+    assert nginx.is_enabled
